6f182495b67e343f45730b1c746d446131c87f35
faustofatec <45613051+faustofatec@users.noreply.github.com>
2021-01-07 16:04:15 -0300

    criando o RequestRepository e RequestStageRepository

---
+++ src/main/java/com/springcourse/repository/RequestRepository.java
@@ -0,0 +1,15 @@
+package com.springcourse.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.springcourse.domain.Request;
+
+@Repository
+public interface RequestRepository extends JpaRepository<Request, Long> {
+	
+	public List<Request> findAllByOwnerId(Long id);
+
+}
