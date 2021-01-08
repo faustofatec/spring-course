3b675438cb089d2455a002a4cca61183d8485ca9
faustofatec <45613051+faustofatec@users.noreply.github.com>
2021-01-08 17:54:17 -0300

    criando requestrepositorytests

--- src/test/java/com/springcourse/repository/UserRepositoryTests.java
+++ src/test/java/com/springcourse/repository/UserRepositoryTests.java
@@ -7,7 +7,6 @@ import java.util.Optional;
 
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
