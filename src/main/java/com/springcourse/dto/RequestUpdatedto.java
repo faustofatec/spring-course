7ae3f39baeffb0491e6dc50beaf40b93dfec124d
faustofatec <45613051+faustofatec@users.noreply.github.com>
2021-01-15 23:54:34 -0300

    Validação do request

---
+++ src/main/java/com/springcourse/dto/RequestUpdatedto.java
@@ -0,0 +1,40 @@
+package com.springcourse.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import com.springcourse.domain.Request;
+import com.springcourse.domain.RequestStage;
+import com.springcourse.domain.User;
+import com.springcourse.domain.enums.RequestState;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RequestUpdatedto {
+	@NotBlank(message = "Subject required")
+	private String subject;
+
+	private String description;
+	
+	private RequestState state; 
+
+	@NotNull(message = "Owner required")
+	private User owner;
+	
+	public Request transformToRequest() {
+		Request request = new Request(null, this.subject, this.description, null, this.state, this.owner, this.stages);
+		return request;
+	}
+	
+	private List<RequestStage> stages = new ArrayList<RequestStage>();
+}
