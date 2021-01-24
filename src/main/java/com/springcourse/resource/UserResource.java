2156f2a070d90e44036c42ef489ce20955daa3b9
faustofatec <45613051+faustofatec@users.noreply.github.com>
2021-01-24 12:49:23 -0300

    criando o jwt

--- src/main/java/com/springcourse/resource/UserResource.java
+++ src/main/java/com/springcourse/resource/UserResource.java
@@ -1,5 +1,8 @@
 package com.springcourse.resource;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.jaas.AuthorityGranter;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,6 +31,7 @@ import com.springcourse.dto.UserUpdateRoledto;
 import com.springcourse.dto.UserUpdatedto;
 import com.springcourse.model.PageModel;
 import com.springcourse.model.PageRequestModel;
+import com.springcourse.security.JwtManager;
 import com.springcourse.service.RequestService;
 import com.springcourse.service.UserService;
 
@@ -41,6 +46,9 @@ public class UserResource {
 	
 	@Autowired
 	private AuthenticationManager authManager;
+	
+	@Autowired
+	private JwtManager jwtManager;
 	
 	@PostMapping
 	public ResponseEntity<User> save(@RequestBody @Valid UserSavedto userdto) {
@@ -73,13 +81,24 @@ public class UserResource {
 	}
 	
 	@PostMapping("/login")
-	public ResponseEntity<User> login(@RequestBody @Valid UserLogindto user) {
+	public ResponseEntity<String> login(@RequestBody @Valid UserLogindto user) {
 		UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(user.getEmail(), user.getPassword());
 		Authentication auth =  authManager.authenticate(token);
 		
 		SecurityContextHolder.getContext().setAuthentication(auth);
 		
-		return ResponseEntity.ok(null);
+		org.springframework.security.core.userdetails.User userSpring = 
+				(org.springframework.security.core.userdetails.User) auth.getPrincipal();
+		
+		String email = userSpring.getUsername();
+		List<String> roles = userSpring.getAuthorities()
+									   .stream()
+									   .map(authority -> authority.getAuthority())
+									   .collect(Collectors.toList());
+		
+		String jwt = jwtManager.createToken(email, roles);
+		
+		return ResponseEntity.ok(jwt);
 	}
 	
 	@GetMapping("/{id}/requests")
