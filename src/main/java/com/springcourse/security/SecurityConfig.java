6abe329ff710c94c1e4e378d56ae4b00ca8e8084
faustofatec <45613051+faustofatec@users.noreply.github.com>
2021-01-25 19:33:18 -0300

    adicionando o accessManager para tratar os prePostEnabled

--- src/main/java/com/springcourse/security/SecurityConfig.java
+++ src/main/java/com/springcourse/security/SecurityConfig.java
@@ -19,7 +19,7 @@ import com.springcourse.service.UserService;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(securedEnabled = true)
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private UserService userService;
