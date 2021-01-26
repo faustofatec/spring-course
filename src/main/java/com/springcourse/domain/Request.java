1330ef601db3d95cbab24bd334521b61469f136f
faustofatec <45613051+faustofatec@users.noreply.github.com>
2021-01-25 22:12:04 -0300

    criação do request file

--- src/main/java/com/springcourse/domain/Request.java
+++ src/main/java/com/springcourse/domain/Request.java
@@ -59,5 +59,9 @@ public class Request implements Serializable {
 	@Getter(onMethod = @__({@JsonIgnore}))
 	@OneToMany(mappedBy = "request")
 	private List<RequestStage> stages = new ArrayList<RequestStage>();
+	
+	@Getter(onMethod = @__({@JsonIgnore}))
+	@OneToMany(mappedBy = "request")
+	private List<RequestFile> files = new ArrayList<RequestFile>();
 
 }
