404e121248d8731979311693446e12b85fc87ead
faustofatec <45613051+faustofatec@users.noreply.github.com>
2021-01-31 11:20:05 -0300

    alterando os page request para um método para tratar isso

--- src/main/java/com/springcourse/service/RequestService.java
+++ src/main/java/com/springcourse/service/RequestService.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
@@ -49,7 +48,7 @@ public class RequestService {
 	}
 	
 	public PageModel<Request> listAllOnLazyMode(PageRequestModel pr) {
-		Pageable pageable = PageRequest.of(pr.getPage(), pr.getSize());
+		Pageable pageable = pr.toSpringPageRequest();
 		Page<Request> page =  requestRepository.findAll(pageable);
 		
 		PageModel<Request> pm = new PageModel<>((int)page.getTotalElements(), page.getSize(), page.getTotalPages(), page.getContent());
@@ -62,7 +61,7 @@ public class RequestService {
 	}
 	
 	public PageModel<Request> listAllOwnerIdOnLazyModel(Long ownerId, PageRequestModel pr) {
-		Pageable pageable = PageRequest.of(pr.getPage(), pr.getSize());
+		Pageable pageable = pr.toSpringPageRequest();
 		Page<Request> page = requestRepository.findAllByOwnerId(ownerId, pageable);
 		
 		PageModel<Request> pm = new PageModel<>((int)page.getTotalElements(), page.getSize(), page.getTotalPages(), page.getContent());
